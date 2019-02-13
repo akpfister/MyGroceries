61abecd7d883a3082daa8659db4bd484ffa7a362
Aaron Pfister <AaronPfister@vpn-campus-161.csuchico.edu>
2019-02-13 12:06:08 -0800

    can insert up to 5 items into list

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include<string>
 using namespace std;
 
+int gatherItems(string (&list)[5], int numItems);
+
 int main()
 {
   string list[5]; //array of 5 strings
@@ -14,6 +16,42 @@ int main()
   cout<<"\n (Q)uit";
   cout<<"\nYour choice (A/Q): ";
   cin>>input;
+  
+
+	if(input == 'A') {
+		numItems = gatherItems(list, numItems);
+	}
+
+	for(int i = 0; i < numItems; i++) {
+		cout << "Item " << i + 1 << ": " << list[i] << endl;
+	}
+		
 
   return 0;
+}
+
+int gatherItems(string (&list)[5], int numItems) {
+	string item = "";
+	cout << "Enter an item:" << endl;
+	while(cin >> item) {
+//		cin.ignore();
+		list[numItems] = item;
+		numItems++;
+		if(numItems == 5) {
+			cout << "Exceeded max number of items" << endl;
+			return numItems;
+		}
+		char input;
+  		cout<<"\nEnter your choice: ";
+  		cout<<"\n (A)dd an item";
+  		cout<<"\n (Q)uit";
+  		cout<<"\nYour choice (A/Q): ";
+		cin >> input;
+		//cin.ignore();
+		if(input == 'Q') {
+			return numItems;
+		}
+		cout << "Enter an item: ";
+	}
+	return numItems;
 }
